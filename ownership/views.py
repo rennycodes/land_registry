29b937b5b4cdcae8a699f9c31dcf028a297d65c4
Renny <rennyemmanuel8@gmail.com>
2024-01-31 09:46:43 -1200

    fixed routes

--- ownership/views.py
+++ ownership/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-# from .models import Landowner, Property
+from .models import Landowner, Property
 from django.http import HttpResponse
 
 # Create your views here.
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 def home(request):
     return HttpResponse('<h1>Blog Home</h1>')
 
-"""
+
 def landowner_list(request):
     landowners = Landowner.objects.all()
     return render(request, 'ownership/landowner_list.html', {'landowners': landowners})
@@ -16,4 +16,4 @@ def property_list(request, landowner_id):
     landowner = get_object_or_404(Landowner, pk=landowner_id)
     properties = Property.objects.filter(landowner=landowner)
     return render(request, 'ownership/property_list.html', {'landowner': landowner, 'properties': properties})
-"""
+
