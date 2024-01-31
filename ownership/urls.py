3b897be7139abca6d45f08109ae6d47c61a92a9a
Renny <rennyemmanuel8@gmail.com>
2024-01-31 09:12:56 -1200

    created home view

--- ownership/urls.py
+++ ownership/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('/', views.home, name='blog-home'),
     path('landowners/', views.landowner_list, name='landowner_list'),
     path('landowners/<int:landowner_id>/', views.property_list, name='property_list'),
 
