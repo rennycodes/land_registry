ca5f15ac95afda792c0e0dbbf99ea2e037f3c38a
Renny <rennyemmanuel8@gmail.com>
2024-01-31 09:16:02 -1200

    changed name from blog-home to ownership-home and added urls path to main urls

--- ownership/urls.py
+++ ownership/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name='blog-home'),
+    path('/', views.home, name='ownership-home'),
     path('landowners/', views.landowner_list, name='landowner_list'),
     path('landowners/<int:landowner_id>/', views.property_list, name='property_list'),
 
