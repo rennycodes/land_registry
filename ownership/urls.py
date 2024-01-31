340a80173506658508bc043458f9d91a3acee1a6
Renny <rennyemmanuel8@gmail.com>
2024-01-31 09:19:05 -1200

    added comments to make server run

--- ownership/urls.py
+++ ownership/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('/', views.home, name='ownership-home'),
-    path('landowners/', views.landowner_list, name='landowner_list'),
-    path('landowners/<int:landowner_id>/', views.property_list, name='property_list'),
+    #path('landowners/', views.landowner_list, name='landowner_list'),
+    #path('landowners/<int:landowner_id>/', views.property_list, name='property_list'),
 
 ]
