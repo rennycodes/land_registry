d9e09950dd9b0b872dceb2e447388b323cac8982
Renny <rennyemmanuel8@gmail.com>
2024-02-01 00:23:01 -1200

    created models

--- ownership/models.py
+++ ownership/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Landowner(models.Model):
+    name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+    
+class Property(models.Model):
+    landowner = models.ForeignKey(Landowner, on_delete=models.CASCADE)
+    address = models.TextField
+
+    def __str__(self):
+        return f"Property of {self.landowner.name}"
