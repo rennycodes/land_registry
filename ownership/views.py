f73be00fd9e9cafcb93254347d414ce980fef430
Renny <rennyemmanuel8@gmail.com>
2024-02-01 09:51:55 -1200

    created ownership views form

--- ownership/views.py
+++ ownership/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Landowner, Property
 from django.http import HttpResponse
+from django.contrib import messages
+from .forms import LandownerForm
 
 # Create your views here.
 
@@ -17,3 +19,13 @@ def property_list(request, landowner_id):
     properties = Property.objects.filter(landowner=landowner)
     return render(request, 'ownership/property_list.html', {'landowner': landowner, 'properties': properties})
 
+def Landowner_Form(request):
+    if request.method == 'POST':
+        form = LandownerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Land Issues Created Already')
+            return redirect('ownership-home')
+    else:
+        form = LandownerForm()
+    return render(request, 'ownership/ownership_form.html', {'form': form})
