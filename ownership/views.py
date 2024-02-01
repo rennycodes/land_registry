1c9647432540c78247e8cf25493403865f57971d
Renny <rennyemmanuel8@gmail.com>
2024-02-01 00:44:41 -1200

    created views

--- ownership/views.py
+++ ownership/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Landowner, Property
 
 # Create your views here.
+
+# ownership/views.py
+def landowner_list(request):
+    landowners = Landowner.objects.all()
+    return render(request, 'ownership/landowner_list.html', {'landowners': landowners})
+
+def property_list(request, landowner_id):
+    landowner = get_object_or_404(Landowner, pk=landowner_id)
+    properties = Property.objects.filter(landowner=landowner)
+    return render(request, 'ownership/property_list.html', {'landowner': landowner, 'properties': properties})
