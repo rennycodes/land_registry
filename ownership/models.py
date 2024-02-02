35953428c39571c8db5a8cf42b495bca4bcc2e64
Renny <rennyemmanuel8@gmail.com>
2024-02-02 10:30:24 -1200

    added: field to address in models.py

--- ownership/models.py
+++ ownership/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Landowner(models.Model):
     name = models.CharField(max_length=100)
     contact_number = models.CharField(max_length=15)
-    address = models.TextField(100)
+    address = models.TextField(max_length = 100)
 
     def __str__(self):
         return self.name
