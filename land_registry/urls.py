db9dd144b484957329580837674f15e859fa02ed
Renny <rennyemmanuel8@gmail.com>
2024-02-02 10:38:11 -1200

    saved urls

--- land_registry/urls.py
+++ land_registry/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ownership/', include('ownership.urls')),
+    path('', include('ownership.urls')),
 
 ]
