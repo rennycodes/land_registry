75fec3f6f6c6ba110273ab24b59531972a7ac254
Renny <rennyemmanuel8@gmail.com>
2024-02-02 10:32:56 -1200

    fixed migrations

--- ownership/models.py
+++ ownership/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Landowner(models.Model):
     name = models.CharField(max_length=100)
     contact_number = models.CharField(max_length=15)
-    address = models.TextField(max_length = 100)
+    address = models.CharField(max_length=255, default='default_address')
 
     def __str__(self):
         return self.name
