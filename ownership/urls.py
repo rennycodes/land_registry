92ab463d23215aca5eb1ab4ce46b8f72b7db4d52
Renny <rennyemmanuel8@gmail.com>
2024-02-03 09:50:45 -1200

    changing url name

--- ownership/urls.py
+++ ownership/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.home, name='ownership-home'),
     path('ownership_form/', views.Landowner_Form, name='ownership-form'),
     path('landowners/', views.landowner_list, name='landowner-list'),
-    path('landowners/<int:landowner_id>/', views.property_list, name='property_list'),
+    path('landowners/<int:landowner_id>/', views.property_list, name='property-list'),
 
 ]
