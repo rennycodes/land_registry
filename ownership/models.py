c79e3620036571b88b4e0cdaf855537b862a51c6
Renny <rennyemmanuel8@gmail.com>
2024-02-06 12:43:38 -1200

    modified models

--- ownership/models.py
+++ ownership/models.py
@@ -3,11 +3,23 @@ from django.db import models
 # Create your models here.
 
 class Landowner(models.Model):
-    name = models.CharField(max_length=100)
-    contact_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=255, default='default_address')
+    id = models.Autofield(primary_key=True)
+    name = models.CharField(max_lenght=255)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField()
+    inputter_name = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.name
-    
 
+class LandProperty(models.Model):
+    id = models.AutoField(primary_key=True)
+    landowner = models.ForeignKey(Landowner, on_delete=models.CASCADE)
+    coordinates = models.PointField(geography=True, blank=True, null=True)
+    land_type = models.CharField(max_length=255)
+    other_details = models.TextField(blank=True, null=True)
+    inputter_name = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Property of {self.landowner.name}"
