87db047447276deb87156ab3de702a67b02f08b2
Renny <rennyemmanuel8@gmail.com>
2024-02-07 14:45:32 -1200

    adjusting forms

--- ownership/views.py
+++ ownership/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Landowner
 from django.http import HttpResponse
 from django.contrib import messages
-from .forms import LandownerForm
+from .forms import LandownerForm, LandPropertyForm
 
 # Create your views here.
 
@@ -25,13 +25,34 @@ def property_list(request, landowner_id):
     properties = Property.objects.filter(landowner=landowner)
     return render(request, 'ownership/property_list.html', {'landowner': landowner, 'properties': properties})
 
+# def Landowner_Form(request):
+#     if request.method == 'POST':
+#         form = LandownerForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, f'Land Issues Created Already')
+#             return redirect('ownership-home')
+#     else:
+#         form = LandownerForm()
+#     return render(request, 'ownership/ownership_form.html', {'form': form})
+
 def Landowner_Form(request):
     if request.method == 'POST':
         form = LandownerForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, f'Land Issues Created Already')
+            landowner = form.save()
             return redirect('ownership-home')
     else:
         form = LandownerForm()
     return render(request, 'ownership/ownership_form.html', {'form': form})
+
+def Landproperty_Form(request):
+    if request.method == 'POST':
+        form = LandPropertyForm(request.POST)
+        if form.is_valid():
+            land_property = form.save()
+            return redirect('ownership-home')
+    else:
+        form = LandPropertyForm()
+    return render(request, 'ownership/ownership_form.html', {'form': form})
+
