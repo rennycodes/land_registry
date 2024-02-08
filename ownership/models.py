9b0131f9b78c9d20ad202deb52818e13c255f815
Renny <rennyemmanuel8@gmail.com>
2024-02-07 15:12:46 -1200

    fixing changes

--- ownership/models.py
+++ ownership/models.py
@@ -13,12 +13,11 @@ class Landowner(models.Model):
         return self.name
 
 class LandProperty(models.Model):
-    id = models.AutoField(primary_key=True)
     landowner = models.ForeignKey(Landowner, on_delete=models.CASCADE)
-    coordinates = models.DecimalField(max_digits=10, decimal_places=6, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
     land_type = models.CharField(max_length=255)
     other_details = models.TextField(blank=True, null=True)
-    inputter_name = models.CharField(max_length=255, blank=True, null=True)
+    
 
     def __str__(self):
         return f"Property of {self.landowner.name}"
