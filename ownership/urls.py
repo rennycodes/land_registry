489f773949f5a16f09b95df3751b4572c8d442e6
Renny <rennyemmanuel8@gmail.com>
2024-02-08 14:38:42 -1200

    adjusted and added url

--- ownership/urls.py
+++ ownership/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='ownership-home'),
     path('about/', views.about, name='ownership-about'),
-    path('ownership_form/', views.Landowner_Form, name='ownership-form'),
+    path('landowner_form/', views.Landowner_Form, name='landowner-form'),
+    path('landproperty_form/', views.Landproperty_Form, name='landproperty-form'),
     path('landowners/', views.landowner_list, name='landowner-list'),
     path('landowners/<int:landowner_id>/', views.property_list, name='property-list'),
 
