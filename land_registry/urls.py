a3b2c48f35c0ad26220804769f81acaa0308997f
Renny <rennyemmanuel8@gmail.com>
2024-02-12 17:03:54 -1200

    made project urls match the home urls in ownership urls

--- land_registry/urls.py
+++ land_registry/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ownership/', include('ownership.urls')),
+    path('', include('ownership.urls')),
 ]
