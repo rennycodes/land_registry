44f3e236d6eefcc58dac622fcb6575526650c49e
Renny <rennyemmanuel8@gmail.com>
2024-02-13 17:25:47 -1200

    creating dummy data

--- ownership/views.py
+++ ownership/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+lands = [
+    {
+        'Name': 'Renny',
+        'location': 'ekehuan road',
+        'size of land': '100ft by 100ft'
+    },
+    {
+        'Name': 'Mike',
+        'location': 'Sapele road',
+        'size of land': '1200ft by 100ft'
+    }
+
+]
+
 # Create your views here.
 
 def home(request):
