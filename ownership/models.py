d7369570c426986bca8b60a69c967e065a6dfd03
Renny <rennyemmanuel8@gmail.com>
2024-02-15 18:48:42 -1200

    added: operator

--- ownership/models.py
+++ ownership/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Land(models.Model):
@@ -20,3 +21,4 @@ class Land(models.Model):
     LGA = models.CharField(max_length=100, default='Unknown')
     state = models.CharField(max_length=100, default='Unknown')
     size_of_land = models.CharField(max_length=50)
+    operator = models.ForeignKey(User, on_delete='PROTECT')
