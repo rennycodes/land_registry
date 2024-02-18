4a22bb7ff94054eb553e809509a2588fc1054834
Renny <rennyemmanuel8@gmail.com>
2024-02-18 01:44:47 -1200

    adjusted: views

--- users/views.py
+++ users/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            messages.success(request, f'{username}, your account has been created! You are now able to log in')
             return redirect('ownership-home')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
