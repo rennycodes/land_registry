02da8679455af405352e8f6f5233612e4ef108fe
Renny <rennyemmanuel8@gmail.com>
2024-02-17 23:45:20 -1200

    added: users app to settings

--- land_registry/settings.py
+++ land_registry/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ownership.apps.OwnershipConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
