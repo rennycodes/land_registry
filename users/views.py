3a0c9b794ac5d9bda7238e1691e0b87e96f65557
Renny <rennyemmanuel8@gmail.com>
2024-02-21 13:26:29 -1200

    changed the success message for creation of account

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'{username}, your account has been created! You are now able to log in')
+            messages.success(request, f'Your account had been created! You are now able to log in')
             return redirect('ownership-home')
     else:
         form = UserRegisterForm()
