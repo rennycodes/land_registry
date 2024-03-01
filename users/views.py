25bb0877ef8464be3b5c1c8626be23689256c8b7
Renny <rennyemmanuel8@gmail.com>
2024-02-29 13:52:42 -1200

    imported: login_required

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
