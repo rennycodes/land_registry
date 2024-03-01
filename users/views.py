41c6add03f4ce65177d9bfe4801b1b1af2484a63
Renny <rennyemmanuel8@gmail.com>
2024-02-29 16:45:27 -1200

    added: messages to updated profile pages

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account had been created! You are now able to log in')
+            messages.success(request, f'Your account has been created! You are now able to log in')
             return redirect('login')
     else:
         form = UserRegisterForm()
@@ -27,6 +27,8 @@ def profile(request):
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.profile)
