1cecaa2251b0efd93ac3e39fbb7c536c7b033f26
Renny <rennyemmanuel8@gmail.com>
2024-02-29 13:54:01 -1200

    added: decorator login_required

--- users/views.py
+++ users/views.py
@@ -17,5 +17,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
