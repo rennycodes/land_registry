e07b3216e38d46a85d74a363718a637d6b40f5eb
Renny <rennyemmanuel8@gmail.com>
2024-02-29 16:49:59 -1200

    adjusted: p_form in else

--- users/views.py
+++ users/views.py
@@ -31,7 +31,7 @@ def profile(request):
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.profile)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
