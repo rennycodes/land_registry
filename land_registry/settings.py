deaf40a7ab405fc70b26a813b0257adc6add9288
Renny <rennyemmanuel8@gmail.com>
2024-02-29 13:35:04 -1200

    added media root and url

--- land_registry/settings.py
+++ land_registry/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = ''
+MEDIA_URL = ''
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
