0c208449ca63ed5abc011473f73d1ef666f89563
Renny <rennyemmanuel8@gmail.com>
2024-03-01 12:10:17 +0100

    created lost view

--- ownership/urls.py
+++ ownership/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import LandListView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='ownership-home'),
+    path('', LandListView.as_view(), name='ownership-home'),
     path('about/', views.about, name='ownership-about'),
 ]
--- ownership/views.py
+++ ownership/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Land
 
 # Create your views here.
@@ -8,6 +9,11 @@ def home(request):
         'lands': Land.objects.all()
     }
     return render(request, 'ownership/home.html', context)
+
+class LandListView(ListView):
+    model = Land
+    template_name = 'ownership/home.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'lands'
 
 def about(request):
     return render(request, 'ownership/about.html', {'title': 'About'})
