fe15c28813c2cec39462650dd0a18caf04d6b7b0
Renny <rennyemmanuel8@gmail.com>
2024-03-01 13:19:18 +0100

    added: landcreateview to url

--- ownership/urls.py
+++ ownership/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LandListView, LandDetailView
+from .views import LandListView, LandDetailView, LandCreateView
 from . import views
 
 urlpatterns = [
     path('', LandListView.as_view(), name='ownership-home'),
     path('land/<int:pk>/', LandDetailView.as_view(), name='land-detail'),
+    path('land/new', LandCreateView.as_view(), name='land-create'),
     path('about/', views.about, name='ownership-about'),
 ]
