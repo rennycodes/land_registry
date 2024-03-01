298aa0eb816d872cb0139a8e15d4a8253ae7f6d2
Renny <rennyemmanuel8@gmail.com>
2024-03-01 13:14:51 +0100

    started create view, added fields

--- ownership/views.py
+++ ownership/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (ListView,
+                                  DetailView,
+                                  CreateView)
 from .models import Land
 
 # Create your views here.
@@ -18,7 +20,15 @@ class LandListView(ListView):
 
 class LandDetailView(DetailView):
     model = Land
-    
+
+class LandCreateView(CreateView):
+    model = Land
+    field = ['land_location', 'X1_point', 'Y1_point',
+             'X2_point', 'Y2_point',
+             'X3_point', 'Y3_point',
+             'X4_point', 'Y4_point',
+             'land_type', 'landmark',
+             'LGA', 'state', 'size_of_land',]
 
 def about(request):
     return render(request, 'ownership/about.html', {'title': 'About'})
