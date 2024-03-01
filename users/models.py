fb5abf585bb11ef5cb287f3c3b6b6a28c0bd40ac
Renny <rennyemmanuel8@gmail.com>
2024-03-01 07:43:20 +0100

    corrected typo

--- users/models.py
+++ users/models.py
@@ -17,5 +17,5 @@ class Profile(models.Model):
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
-            img.thumbnaul(output_size)
+            img.thumbnail(output_size)
             img.save(self.image.path)
