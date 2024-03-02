78f0f55b16edfbe1c2e80c77371f11dc2cf2becf
Renny <rennyemmanuel8@gmail.com>
2024-03-02 16:50:51 +0100

    added: userlandlistview to urls

--- ownership/urls.py
+++ ownership/urls.py
@@ -3,11 +3,13 @@ from .views import (LandListView,
                     LandDetailView,
                     LandCreateView,
                     LandUpdateView,
-                    LandDeleteView)
+                    LandDeleteView,
+                    UserLandListView)
 from . import views
 
 urlpatterns = [
     path('', LandListView.as_view(), name='ownership-home'),
+    path('user/<str:username>', UserLandListView.as_view(), name='user-lands'),
     path('land/<int:pk>/', LandDetailView.as_view(), name='land-detail'),
     path('land/new', LandCreateView.as_view(), name='land-create'),
     path('land/<int:pk>/update', LandUpdateView.as_view(), name='land-update'),
