9dd5ce54b3b0773620ca4359a6b34a6b877095cc
Renny <rennyemmanuel8@gmail.com>
2024-03-02 03:43:32 +0100

    added: update view to urls

--- ownership/urls.py
+++ ownership/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import LandListView, LandDetailView, LandCreateView
+from .views import (LandListView,
+                    LandDetailView,
+                    LandCreateView,
+                    LandUpdateView)
 from . import views
 
 urlpatterns = [
     path('', LandListView.as_view(), name='ownership-home'),
     path('land/<int:pk>/', LandDetailView.as_view(), name='land-detail'),
     path('land/new', LandCreateView.as_view(), name='land-create'),
+    path('land/<int:pk>/update', LandUpdateView.as_view(), name='land-update'),
     path('about/', views.about, name='ownership-about'),
 ]
