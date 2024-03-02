8b21c16c4430289c8ba7009ff2ba5145a54e2839
Renny <rennyemmanuel8@gmail.com>
2024-03-02 03:40:33 +0100

    added: update view

--- ownership/views.py
+++ ownership/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView)
+                                  CreateView,
+                                  UpdateView)
 from .models import Land
 
 # Create your views here.
@@ -21,7 +23,22 @@ class LandListView(ListView):
 class LandDetailView(DetailView):
     model = Land
 
-class LandCreateView(CreateView):
+class LandCreateView(LoginRequiredMixin, CreateView):
+    model = Land
+    fields = ['name', 'residential_address',
+              'phone_number', 'email',
+              'land_location', 'X1_point', 'Y1_point',
+              'X2_point', 'Y2_point',
+              'X3_point', 'Y3_point',
+              'X4_point', 'Y4_point',
+              'land_type', 'landmark',
+              'LGA', 'state', 'size_of_land',]
+    
+    def form_valid(self, form):
+        form.instance.operator = self.request.user
+        return super().form_valid(form)
+
+class LandUpdateView(LoginRequiredMixin, UpdateView):
     model = Land
     fields = ['name', 'residential_address',
               'phone_number', 'email',
